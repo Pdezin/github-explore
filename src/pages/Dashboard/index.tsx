8c0d06761ad8b2d860ae16b0e88931690ece4f79
Pedro Jardim <pedro200219@outlook.com>
2021-10-09 15:57:51 -0300

    Página de repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, useEffect, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
 
-import { Title, Form, Repositories } from "./styles";
+import { Title, Form, Repositories, Error } from "./styles";
 import LogoImg from "../../assets/logo.svg";
 
 interface Repository {
@@ -16,18 +17,44 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState("");
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [inputError, setInputError] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      "@GithubExplorer:repositories"
+    );
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      "@GithubExplorer:repositories",
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>
   ): Promise<void> {
     event.preventDefault();
-    const response = await api.get<Repository>(`repos/${newRepo}`);
+    if (!newRepo) {
+      setInputError("Digite o autor/nome do repositório");
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
 
-    const repository = response.data;
+      const repository = response.data;
 
-    setRepositories((prev) => [...prev, repository]);
-    setNewRepo("");
+      setRepositories((prev) => [...prev, repository]);
+      setInputError("");
+      setNewRepo("");
+    } catch (err) {
+      setInputError("Erro na busca por esse repositório");
+    }
   }
 
   return (
@@ -35,7 +62,7 @@ const Dashboard: React.FC = () => {
       <img src={LogoImg} alt="Github Explore" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           placeholder="Digite o nome do repositório"
           value={newRepo}
@@ -44,9 +71,14 @@ const Dashboard: React.FC = () => {
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="test">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -56,7 +88,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={30} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import Logo from "../../assets/logo.svg";
+
+import { Header, RepositoryInfo, Issues } from "./styles";
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={Logo} alt="Github Explore" />
+        <Link to="/">
+          Voltar <FiChevronLeft size={20} />
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/82681415?v=4"
+            alt="teste"
+          />
+          <div>
+            <strong>user/repositorio</strong>
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sdadads">
+          <div>
+            <strong>Dasdasddad</strong>
+            <p>Dasdasddad</p>
+          </div>
+          <FiChevronRight size={30} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
